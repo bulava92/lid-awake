ee4afd285705a866664abc8cde34784bfd7a8df5
bulava92 <96921934+bulava92@users.noreply.github.com>
2026-07-25 15:33:25 +0500

    Add lid-close sound controls to settings menu

--- Sources/LidAwakeApp/main.swift
+++ Sources/LidAwakeApp/main.swift
@@ -63,6 +63,21 @@ final class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         lock.state = settings.lockOnLidClose ? .on : .off
         settingsMenu.addItem(lock)
 
+        let sound = item(t("Play sound when lid closes", "Звук при закрытии крышки"), #selector(toggleLidCloseSound))
+        sound.state = settings.soundOnLidClose ? .on : .off
+        settingsMenu.addItem(sound)
+
+        let soundVolumeMenu = NSMenu()
+        for value in [25, 50, 75, 100] {
+            let entry = item("\(value)%", #selector(setLidCloseSoundVolume(_:)))
+            entry.tag = value
+            entry.state = settings.lidCloseSoundVolume == value ? .on : .off
+            soundVolumeMenu.addItem(entry)
+        }
+        let soundVolumeItem = submenu(t("Sound volume", "Громкость звука"), soundVolumeMenu)
+        soundVolumeItem.isEnabled = settings.soundOnLidClose
+        settingsMenu.addItem(soundVolumeItem)
+
         let thermal = item(t("Thermal protection", "Защита от перегрева"), #selector(toggleThermal))
         thermal.state = settings.thermalProtection ? .on : .off
         settingsMenu.addItem(thermal)
@@ -180,15 +195,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         rebuildMenu()
     }
 
-    private func updateSettings(_ mutate: (inout LidAwakeSettings) -> Void) throws {
-        var settings = controller.loadSettings()
-        mutate(&settings)
-        settings.batteryLimit = min(max(settings.batteryLimit, 0), 100)
-        settings.maxDuration = max(settings.maxDuration, 60)
-        try controller.saveSettings(settings)
-        _ = try controller.reconcile()
-    }
-
     private func showAlert(title: String, message: String, style: NSAlert.Style = .informational) {
         let alert = NSAlert()
         alert.alertStyle = style
@@ -200,16 +206,23 @@ final class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     @objc private func enablePermanent() { perform { try controller.requestEnabled() } }
     @objc private func enableTemporary(_ sender: NSMenuItem) { perform { try controller.requestTemporary(duration: sender.tag) } }
     @objc private func disable() { perform { try controller.requestDisabled() } }
-    @objc private func toggleAC() { perform { try updateSettings { $0.acOnly.toggle() } } }
-    @objc private func toggleLockOnClose() { perform { try updateSettings { $0.lockOnLidClose.toggle() } } }
-    @objc private func toggleThermal() { perform { try updateSettings { $0.thermalProtection.toggle() } } }
-    @objc private func toggleNotifications() { perform { try updateSettings { $0.notifications.toggle() } } }
+    @objc private func toggleAC() { perform { try controller.update { $0.acOnly.toggle() } } }
+    @objc private func toggleLockOnClose() { perform { try controller.update { $0.lockOnLidClose.toggle() } } }
+    @objc private func toggleLidCloseSound() { perform { try controller.update { $0.soundOnLidClose.toggle() } } }
+    @objc private func setLidCloseSoundVolume(_ sender: NSMenuItem) {
+        perform {
+            try controller.update { $0.lidCloseSoundVolume = sender.tag }
+            _ = controller.playLidCloseSound(volumePercent: sender.tag)
+        }
+    }
+    @objc private func toggleThermal() { perform { try controller.update { $0.thermalProtection.toggle() } } }
+    @objc private func toggleNotifications() { perform { try controller.update { $0.notifications.toggle() } } }
     @objc private func toggleLogin() { perform { try controller.update { $0.launchAtLogin.toggle() } } }
-    @objc private func setBatteryLimit(_ sender: NSMenuItem) { perform { try updateSettings { $0.batteryLimit = sender.tag } } }
+    @objc private func setBatteryLimit(_ sender: NSMenuItem) { perform { try controller.update { $0.batteryLimit = sender.tag } } }
 
     @objc private func setMaxDuration(_ sender: NSMenuItem) {
         perform {
-            try updateSettings {
+            try controller.update {
                 $0.maxDuration = sender.tag
                 if $0.requested, $0.expiresAt != nil {
                     $0.expiresAt = Date().addingTimeInterval(TimeInterval(sender.tag))
